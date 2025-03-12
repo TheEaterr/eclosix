d1e157f32166c5216118560beda8f6c7e45b97e1
Paul Breuil <tykapl.breuil@gmail.com>
2025-03-12 18:51:31 +0100

    fix: make loading logo adapt to theme

--- front/src/lib/components/LoadingLogo.svelte
+++ front/src/lib/components/LoadingLogo.svelte
@@ -4,48 +4,48 @@
 <svg width="200" height="200" viewBox="0 0 270.93334 270.93333" version="1.1" id="svg1">
 	<path
 		class="animate-pulse"
-		style="animation-delay:600ms;fill:#d0dddb;fill-opacity:1;stroke:#115e59;stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
+		style="animation-delay:600ms;fill:var(--color-primary-content);fill-opacity:1;stroke:var(--color-primary);stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
 		id="path1"
 		d="m 155,99.999999 20.31089,34.820511 -20,35 L 115,170 94.689109,135.17949 l 20.000001,-35 z"
 	/>
 	<path
 		class="animate-pulse"
-		style="animation-delay:500ms;fill:#bbbbcc;fill-opacity:1;stroke:#4b5563;stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
+		style="animation-delay:500ms;fill:var(--color-neutral-content);fill-opacity:1;stroke:var(--color-neutral);stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
 		id="path1-5"
 		d="m 155,99.999999 20.31089,34.820511 -20,35 L 115,170 94.689109,135.17949 l 20.000001,-35 z"
 		transform="translate(0,-87.963458)"
 	/>
 	<path
 		class="animate-pulse"
-		style="animation-delay:400ms;fill:#bbbbcc;fill-opacity:1;stroke:#4b5563;stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
+		style="animation-delay:400ms;fill:var(--color-neutral-content);fill-opacity:1;stroke:var(--color-neutral);stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
 		id="path1-5-2"
 		d="m 155,99.999999 20.31089,34.820511 -20,35 L 115,170 94.689109,135.17949 l 20.000001,-35 z"
 		transform="translate(0.46666667,87.963458)"
 	/>
 	<path
 		class="animate-pulse"
-		style="animation-delay:300ms;fill:#bbbbcc;fill-opacity:1;stroke:#4b5563;stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
+		style="animation-delay:300ms;fill:var(--color-neutral-content);fill-opacity:1;stroke:var(--color-neutral);stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
 		id="path1-5-2-6"
 		d="m 155,99.999999 20.31089,34.820511 -20,35 L 115,170 94.689109,135.17949 l 20.000001,-35 z"
 		transform="translate(75.310886,-44.246141)"
 	/>
 	<path
 		class="animate-pulse"
-		style="animation-delay:200ms;fill:#e5dcff;fill-opacity:1;stroke:#4f46e5;stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
+		style="animation-delay:200ms;fill:var(--color-secondary-content);fill-opacity:1;stroke:var(--color-secondary);stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
 		id="path1-5-2-7"
 		d="m 155,99.999999 20.31089,34.820511 -20,35 L 115,170 94.689109,135.17949 l 20.000001,-35 z"
 		transform="translate(75.310886,42.320511)"
 	/>
 	<path
 		class="animate-pulse"
-		style="animation-delay:100ms;fill:#bbbbcc;fill-opacity:1;stroke:#4b5563;stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
+		style="animation-delay:100ms;fill:var(--color-neutral-content);fill-opacity:1;stroke:var(--color-neutral);stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
 		id="path1-5-2-7-6"
 		d="m 155,99.999999 20.31089,34.820511 -20,35 L 115,170 94.689109,135.17949 l 20.000001,-35 z"
 		transform="translate(-73.783632,42.320511)"
 	/>
 	<path
 		class="animate-pulse"
-		style="fill:#bbbbcc;fill-opacity:1;stroke:#4b5563;stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
+		style="fill:var(--color-neutral-content);fill-opacity:1;stroke:var(--color-neutral);stroke-width:5;stroke-dasharray:none;stroke-opacity:1"
 		id="path1-5-2-7-9"
 		d="m 155,99.999999 20.31089,34.820511 -20,35 L 115,170 94.689109,135.17949 l 20.000001,-35 z"
 		transform="translate(-73.783632,-44.246141)"
